2b3c05ff13b9c6cc442ac2324a16d735cffa1eac
C VAIBHAV <67633271+bunnykek@users.noreply.github.com>
2024-04-19 12:43:50 +0530

    '.ENVR'

--- src/lib/ABI.js
+++ src/lib/ABI.js
@@ -1,1 +1,94 @@
-export const CONTRACT_ABI=[];
+export const CONTRACT_ABI=[
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "user",
+                "type": "address"
+            },
+            {
+                "internalType": "string",
+                "name": "CID",
+                "type": "string"
+            },
+            {
+                "internalType": "uint8",
+                "name": "flag",
+                "type": "uint8"
+            }
+        ],
+        "name": "commitFilePermission",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "host",
+                "type": "address"
+            }
+        ],
+        "name": "getCommonFilescid",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "CID",
+                "type": "string"
+            }
+        ],
+        "name": "getKey",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "listMyFiles",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "CID",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "key",
+                "type": "string"
+            }
+        ],
+        "name": "uploadCID",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
